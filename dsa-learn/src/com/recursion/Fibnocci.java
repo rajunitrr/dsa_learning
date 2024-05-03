64aa18b44e606abb1149b5113f9c428fe2416727
Raju Motapothula <raju.motapothula@ivycomptech.com>
2024-05-03 12:38:05 +0530

    Add existing file

--- dsa-learn/src/com/recursion/Fibnocci.java
+++ dsa-learn/src/com/recursion/Fibnocci.java
@@ -5,8 +5,11 @@ public class Fibnocci {
 	public static void main(String[] args) {
 
 		int n = 5;
+		long startTime = System.currentTimeMillis();
 		System.out.println("fib of " + n + " is " + fib(n));
+		long duration = System.currentTimeMillis() - startTime;
 
+		System.out.println("Duration of program execution " + duration);
 	}
 
 	private static int fib(int n) {
